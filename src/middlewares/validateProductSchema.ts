97f141fb36de8f418ea67b67d8349daf891116c6
mateussousaa <mateussousa20@outlook.com>
2022-11-07 15:27:41 -0300

    feat: creating validations of the post method - products [ requirement 06 ]

---
+++ src/middlewares/validateProductSchema.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from 'express';
+import Joi from 'joi';
+import Product from '../interfaces/product.interface';
+import HttpException from '../utils/httpException';
+
+const validateSchema = (product: Product) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+    amount: Joi.string().min(3).required(),
+  });
+  return schema.validate(product);
+};
+
+const validateProductSchema = (req: Request, res: Response, next: NextFunction) => {
+  const { name, amount } = req.body;
+  
+  if (!name) throw new HttpException(400, '"name" is required');
+  if (!amount) throw new HttpException(400, '"amount" is required');
+  const { error } = validateSchema({ name, amount });
+  if (error) throw new HttpException(422, error.details[0].message);
+  next();
+};
+
+export default validateProductSchema;
--- src/routes/product.router.ts
+++ src/routes/product.router.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { productController } from '../controllers';
+import validateProductSchema from '../middlewares/validateProductSchema';
 
 const productRouter = Router();
 
-productRouter.post('/', productController.insertProduct);
+productRouter.post('/', validateProductSchema, productController.insertProduct);
 
 productRouter.get('/', productController.getProducts);
 
